1e4bafd0e1bee6c2f5295edbdceacda12f5df2fb
BilelMH <bilel.mh1997@gmail.com>
2023-06-16 19:01:11 +0100

    Router

--- src/Compouning/Mouviecard/Moviecard.js
+++ src/Compouning/Mouviecard/Moviecard.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import MovieRating from '../Raiting/Raiting';
-const mouvieCard = ({mouvie}) => {
+import { useNavigate } from 'react-router-dom';
+import './Moviecard.css';
+const Moviecard = ({mouvie}) => {
+  const navigate = useNavigate ();
   return (
     <div className='Cards' style={{marginRight:'20px'}}>
       <Card style={{ width: '18rem' , margin :'30px auto' }}>
@@ -10,6 +13,8 @@ const mouvieCard = ({mouvie}) => {
         <Card.Title>{mouvie.title}</Card.Title>
         <Card.Text> {mouvie.description} </Card.Text>
         <MovieRating movieRating={mouvie.rate} isMovieRating={true}/>
+        <button className='click' onClick={()=>navigate (`/Description/${mouvie.id}`)}>Description </button>
+      <button className='click'onClick={()=>navigate(`/Trailer/${mouvie.id}`)}>Trailer</button>
       </Card.Body>
     </Card>
 
@@ -17,4 +22,4 @@ const mouvieCard = ({mouvie}) => {
   )
 }
 
-export default mouvieCard
+export default Moviecard
